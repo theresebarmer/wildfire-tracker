7e40ef3200793ec954387f2a82f60d457d17f6f0
Therése Barmer <therese.barmer@gmail.com>
2020-12-26 13:47:40 +0100

    add everything

---
+++ src/components/Map.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
+import LocationInfoBox from './LocationInfoBox';
+
+const Map = ({ eventData, center, zoom }) => {
+  const [locationInfo, setLocationInfo]  = useState(null);
+  const markers = eventData.map(event => {
+
+    if(event.title.includes("Wildfire")) {
+      return <LocationMarker
+        lat={ event.geometries[0].coordinates[1] }
+        lng={ event.geometries[0].coordinates[0] }
+        onClick={() => setLocationInfo({ id: event.id, title: event.title })}
+      />;
+    }
+    return null;
+  })
+
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDx6OfHlBCBvZwLXhbr4ZpJUywEoapJxGk' }}
+        defaultCenter={ center }
+        defaultZoom={ zoom }
+      >
+        {markers}
+      </GoogleMapReact>
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
+    </div>
+  )
+}
+
+Map.defaultProps = {
+  center: {
+    lat: 42.3265,
+    lng: -122.8756
+  },
+  zoom: 6
+}
+
+export default Map;
